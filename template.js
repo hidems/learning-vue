28a555a9bc7596095d9ee85b6d4d3b3b492bfb0a
hidems <hidemiura.arms@gmail.com>
2021-02-03 17:11:52 +0100

    v-html directive

--- template.js
+++ template.js
@@ -1,8 +1,9 @@
 var obj = {
-  msg: 'Guten Tag!'
+  msg: 'Guten Tag!',
+    rawHtml: '<div style="color:red">Auf widersehen</div>',
 }
 
 var vm = new Vue({
   el: '#app',
-  data: obj
+  data: obj,
 })
