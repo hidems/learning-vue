773fdf9806f7d694be6d9a5ebcecf452ff0d63e1
hidems <hidemiura.arms@gmail.com>
2021-02-03 17:31:11 +0100

    JS code availability

--- template.js
+++ template.js
@@ -3,6 +3,10 @@ var obj = {
   rawHtml: '<div style="color:red">Auf widersehen</div>',
   myId: "id_3",
   bool: true,
+  number: 100,
+  ok: undefined,
+  message: "Gruess Gott",
+  id: 355,
 }
 
 var vm = new Vue({
