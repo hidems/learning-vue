b8fb712eeb08524b36606b486fbb4e48b8239dea
hidems <miura.eleven@gmail.com>
2021-02-16 15:42:05 +0100

    object, v-for, list rendering

--- list_redering.js
+++ list_redering.js
@@ -10,7 +10,12 @@ var vm = new Vue({
       {
         name: 'Hans',
         message: 'Hallo'
+      },
+    ],
+    object: {
+        title: 'How to do lists in Vue',
+        author: 'Jane Doe',
+        publishedAt: '2016-04-10'
       }
-    ]
   }
 })
