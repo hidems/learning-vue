fa1201e3a3f0b40cf03b4013d266680339bd0e54
hidems <miura.eleven@gmail.com>
2021-02-18 23:04:55 +0100

    method event handler, event handling

--- event_handling.js
+++ event_handling.js
@@ -1,6 +1,17 @@
 var example1 = new Vue({
   el: '#app',
   data: {
-    counter: 0
+    counter: 0,
+    name: 'Vue.js',
+  },
+  methods: {
+    greet: function (event) {
+      // メソッド内の `this` は、 Vue インスタンスを参照します
+      alert('Hello ' + this.name + '!')
+      // `event` は、ネイティブ DOM イベントです
+      if (event) {
+        alert(event.target.tagName)
+      }
+    }
   }
 })
