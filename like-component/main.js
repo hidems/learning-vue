effb44d4b93d6b139315d5b16c5aab5d47cbe6ad
hidems <miura.eleven@gmail.com>
2023-01-25 23:01:53 +0100

    Like-component, props and emit

--- like-component/main.js
+++ like-component/main.js
@@ -1,8 +1,32 @@
 Vue.component('like-component', {
-  props: ['post'],
-  template: '<button>Like</button>'
+  props: {
+    message: {
+      type: String,
+      default: 'Like'
+    }
+  },
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button @click="countUp">{{ message }} {{ count }}</button>',
+  methods: {
+    countUp: function () {
+      this.count++;
+      this.$emit('increment');
+    }
+  }
 })
 
 new Vue({
   el: '#app',
+  data: {
+    total: 0
+  },
+  methods: {
+    incrementTotal: function () {
+      this.total++;
+    }
+  }
 });
